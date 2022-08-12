3c1a4702635f79087b5831c31243bf37c8271a87
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-08-12 23:59:51 +0530

    leafleat-initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./App.css";
+const App = () => {
+	const position = [20.5937, 78.9629];
+	const zoom = 5;
+	return (
+		<MapContainer center={position} zoom={zoom}>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+		</MapContainer>
+	);
+};
 
 export default App;
