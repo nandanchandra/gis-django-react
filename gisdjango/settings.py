c8a0a75a79f4cca58cc08e71770079b50b297a71
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-08-31 20:45:52 +0530

    static-files-served

--- gisdjango/settings.py
+++ gisdjango/settings.py
@@ -140,8 +140,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
+
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATICFILES_DIRS = []
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = str(BASE_DIR / "mediafiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
